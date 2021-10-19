ab17dd2cc3fbed6ea75af0e175d4fc8b42f511fd
shantanukamath <shantanukamath@live.in>
2021-10-19 03:04:43 -0400

    Added winner and secret

--- f1_goat.py
+++ f1_goat.py
@@ -16,6 +16,7 @@ st.text("Well this should help you!")
 st.title('Who is the Formula 1 GOAT (Greatest Of All Time) ?')
 st.text('And now I can argue (or not) with facts. Yes facts. Not just anecdotes and glamour, but cold hard FACTS.')
 
+flag=False
 @st.cache
 def load_data():
     return pd.read_csv("./race_results_mega.csv")
@@ -24,10 +25,11 @@ def load_data():
 def get_driver_image(driver_name):
     driver_name = driver_name.replace(' ', '%20')
     conn = http.client.HTTPSConnection("api-formula-1.p.rapidapi.com")
+        # 'x-rapidapi-key': "7e627ff4bemsh076cf2f715b08f9p1ede64jsnb7e5375d9a8f"
 
     headers = {
         'x-rapidapi-host': "api-formula-1.p.rapidapi.com",
-        'x-rapidapi-key': "7e627ff4bemsh076cf2f715b08f9p1ede64jsnb7e5375d9a8f"
+        'x-rapidapi-key': st.secret("rapidapi_key")
         }
 
     conn.request("GET", "/drivers?search={}".format(driver_name), headers=headers)
@@ -55,7 +57,7 @@ def map(data, lat, lon, zoom, column):
             data=data,
             get_position='[lon, lat]',
             radius=100,
-            elevation_scale=1,
+            elevation_scale=4,
             elevation_range=[0, 100],
             pickable=True,
             extruded=True,
@@ -63,6 +65,16 @@ def map(data, lat, lon, zoom, column):
      ],
  ))
 
+def winner():
+    flag=True
+    st.balloons()
+    col3.title("Lewis Hamilton")
+    col3.image(resizedImgs[0])
+
+def calculate_scores(scores):
+    max_val = max(scores)
+    for idx, val in enumerate(scores):
+        total_scores[idx] += val/max_val*100
 
 # Load Data
 # data_load_state = st.subheader('Calling Hamilton, Schumacher and Alonso, asking for their stats...')
@@ -77,7 +89,8 @@ subheading.subheader('Race data from {}-{}'.format(year_values[0], year_values[1
 
 # Query Drivers
 st.subheader('Select drivers')
-selected_drivers = st.multiselect("Select the drivers for comparison:", data.driver_name.unique(), ['Lewis Hamilton', 'Michael Schumacher', 'Kimi Raikkonen', 'Fernando Alonso'])
+# ['Lewis Hamilton', 'Michael Schumacher', 'Kimi Raikkonen', 'Fernando Alonso']
+selected_drivers = st.multiselect("Select the drivers for comparison:", data.driver_name.unique(), ['Nico Rosberg','Kimi Raikkonen', 'Fernando Alonso'])
 data = data[data.driver_name.isin(selected_drivers)]
 data = data[data.race_year.between(year_values[0], year_values[1], inclusive=True)]
 
@@ -85,6 +98,9 @@ data = data[data.race_year.between(year_values[0], year_values[1], inclusive=Tru
 st.write(data)
 
 # Display driver image
+resizedImgs = []
+total_scores = [0 for _ in selected_drivers]
+
 cols = st.columns(len(selected_drivers))
 for idx, name in enumerate(selected_drivers):
     url = get_driver_image(name)
@@ -97,9 +113,11 @@ for idx, name in enumerate(selected_drivers):
     resizedImg = img.resize((225, 325), Image.ANTIALIAS)
     cols[idx].header(selected_drivers[idx])
     cols[idx].image(resizedImg)
+    resizedImgs.append(resizedImg)
 
 # Statistics
 cols = st.columns(len(selected_drivers))
+win_rates, podium_rates, pole_rates = [], [], []
 for idx, name in enumerate(selected_drivers):
     wins = len(data[(data.driver_name == name) & (data.finish_position == 1)].index)
     podiums = len(data[(data.driver_name == name) & (data.finish_position.isin([1,2,3]))].index)
@@ -117,6 +135,13 @@ for idx, name in enumerate(selected_drivers):
     cols[idx].metric(label="Race Win Rate", value="{:.2f} %".format(win_rate))
     cols[idx].metric(label="Podium Finish Rate", value="{:.2f} %".format(podium_rate))
     cols[idx].metric(label="Qualifying Pole Rate", value="{:.2f} %".format(pole_rate))
+    win_rates.append(win_rate)
+    podium_rates.append(podium_rate)
+    pole_rates.append(pole_rate)
+
+# calculate_scores(win_rates)
+calculate_scores(podium_rates)
+calculate_scores(pole_rates)
 
 # Select circuits
 st.subheader("Select your favourite circuit")
@@ -124,6 +149,10 @@ selected_circuit = st.selectbox("Select a race circuits for comparison:", data.c
 cols = st.columns(len(selected_drivers))
 
 # Display circuits
+win_rates = []
+points = []
+avg_points = []
+
 cols = st.columns(len(selected_drivers))
 for idx, name in enumerate(selected_drivers):
     driver_data = data[(data.driver_name == name)]
@@ -137,7 +166,23 @@ for idx, name in enumerate(selected_drivers):
     cols[idx].subheader('Positions')
     cols[idx].line_chart(race_data[['start_position', 'finish_position', 'position_gained']])
     total_points = race_data['points'].sum()
-    avg_points = race_data['points'].mean()
+    wins = len(race_data[race_data['finish_position']== 1])
+    avg_point = race_data['points'].mean()
     delta = race_data[race_data.index == race_data.index[-1]]['points'].mean()
+    cols[idx].metric(label="Wins on this circuit", value="{}".format(wins))
     cols[idx].metric(label="Total Points earned on this circuit", value="{}".format(total_points), delta=delta)
-    cols[idx].metric(label="Average points earned per race", value="{:.1f}".format(avg_points))
+    cols[idx].metric(label="Average points earned per race", value="{:.1f}".format(avg_point))
+    win_rates.append(win_rate)
+    points.append(total_points)
+    avg_points.append(avg_point)
+
+calculate_scores(win_rates)
+calculate_scores(points)
+calculate_scores(avg_points)
+
+idx = total_scores.index(max(total_scores))
+_, col3, _ = st.columns([1,1,1])
+col3.subheader("And the GOAT (Based on the arbitrary scoring) is...")
+col3.title(selected_drivers[idx])
+col3.image(resizedImgs[idx])
+col3.button("Celebrate!", on_click=st.balloons)
