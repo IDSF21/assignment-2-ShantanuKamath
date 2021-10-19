c2a45a3daee15174edb71de566649f869fba2fc1
shantanukamath <shantanukamath@live.in>
2021-10-18 21:21:40 -0400

    Added some statistics

--- f1_goat.py
+++ f1_goat.py
@@ -9,7 +9,8 @@ from PIL import Image
 from io import BytesIO
 st.set_page_config(layout="wide")
 
-st.title('Formula1 - Greatest of all time')
+st.title('Formula1')
+st.title('Who is the GOAT - Greatest Of All Time')
 
 @st.cache
 def load_data():
@@ -54,20 +55,22 @@ def map(data):
         ]
     ))
 
-data_load_state = st.text('Talking to all F1 Drivers...')
+data_load_state = st.text('Ready Set Lights')
 data = load_data()
-data_load_state.text('Talking to all F1 Drivers......done!')
+data_load_state.text('Ready Set Lights...done!')
 
-st.subheader('Formula 1 - Race data from 1950-2021')
-# st.write(data)
+subheading = st.subheader('Formula 1 - Race data from {}-{}'.format(1950, 2021))
+year_values = st.slider('Select data range', 1950, 2021, (1950, 2021))
+subheading.subheader('Formula 1 - Race data from {}-{}'.format(year_values[0], year_values[1]))
 
-selected_options = st.multiselect("Select one or more options:", data.driver_name.unique(), ['Lewis Hamilton', 'Michael Schumacher', 'Nico Rosberg', 'Max Verstappen', 'Fernando Alonso'])
+selected_options = st.multiselect("Select the drivers for comparison:", data.driver_name.unique(), ['Lewis Hamilton', 'Michael Schumacher', 'Nico Rosberg', 'Max Verstappen', 'Fernando Alonso'])
 
 data = data[data.driver_name.isin(selected_options)]
+data = data[data.race_year.between(year_values[0], year_values[1], inclusive=True)]
 st.write(data)
 
 resizedImages = []
-cols = st.beta_columns(len(selected_options))
+cols = st.columns(len(selected_options))
 for idx, name in enumerate(selected_options):
     url = get_driver_image(name)
     try:
@@ -81,7 +84,24 @@ for idx, name in enumerate(selected_options):
     cols[idx].header(selected_options[idx])
     cols[idx].image(resizedImg)
 
-# st.image(resizedImages, caption=selected_options)
+cols = st.columns(len(selected_options))
+for idx, name in enumerate(selected_options):
+    wins = len(data[(data.driver_name == name) & (data.finish_position == 1)].index)
+    podiums = len(data[(data.driver_name == name) & (data.finish_position.isin([1,2,3]))].index)
+    poles = len(data[(data.driver_name == name) & (data.start_position.isin([1]))].index)
+    total = len(data[(data.driver_name == name)].index)
+
+    if total == 0:
+        win_rate = 0
+        podium_rate = 0
+        pole_rate =  0
+    else:
+        win_rate = wins*100/total
+        podium_rate = podiums*100/total
+        pole_rate = poles*100/total
+    cols[idx].metric(label="Race Win Rate", value="{:.2f} %".format(win_rate))
+    cols[idx].metric(label="Podium Finish Rate", value="{:.2f} %".format(podium_rate))
+    cols[idx].metric(label="Qualifying Pole Rate", value="{:.2f} %".format(pole_rate))
 # map(data)
 # if all_options:
 #     selected_options = ['A', 'B', 'C']
